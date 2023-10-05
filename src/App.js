02024565e5b3f40317b6ba27fea26b617fc5a4fa
Nishabhooshan <Bhooshannisha@gmail.com>
2023-10-05 11:44:08 +0530

    added mentioned component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MentionInput from './MentionInput';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MentionInput/>
+      </div>
+  );
+}
+
+export default App
